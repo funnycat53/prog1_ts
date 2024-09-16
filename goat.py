23825a5a2b0c4da99dee89ae1d7ce3533709e349
funnycat53 <patriks.jkb@gmail.com>
2024-09-16 09:45:10 +0300

    Create goat.py

---
+++ goat.py
@@ -0,0 +1,46 @@
+"""
+Patriks Jēkabsons
+11.09.2024
+Izveidot programmu, kas aprēķina pirkumu bez pvn un pašu pvn vērtību
+
+"""
+
+# pvn = 0.79
+# cikIztereja = float(input("Cik jūs naudu iztērējāt?"))
+# bezPVN = cikIztereja * pvn
+
+# print("Summa bez PVN:", bezPVN)
+
+
+"""
+
+Izveidot programmu, kur lietotājs secīgi var pabeigt 5 teikumus
+
+"""
+
+# Teik1 = input("Mani sauc - ")
+# Teik2 = input("Man patīk - ")
+# Teik3 = input("Man vecums - ")
+# Teik4 = input("Es nodarbojos ar - ")
+# Teik5 = input("Mana mīļakā krāsa ir - ")
+
+"""
+
+Izveidot programmu, kas pārbauda vai lietotājam ir vairāk kā 18 gadi (tikai gadu).
+
+"""
+
+# gads = 2024
+# dzGads = 2006
+
+# if gads - dzGads >= 18:
+#     print("Lietotājam ir 18 gadu")
+# else:
+#     print("Lietotājam nav 18 gadu")
+
+"""
+
+Izveidot programmu, kuru var atkārtot vairākas reizes
+
+"""
+
