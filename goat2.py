389ec1c23747d4bcf0026b31fce5e7706ad702c6
funnycat53 <patriks.jkb@gmail.com>
2024-09-23 16:33:27 +0300

    Update goat2.py

--- goat2.py
+++ goat2.py
@@ -14,13 +14,57 @@ Tiek izvadīts, cik norādīto burtu ir dotajā vārdā
 #         atb+= 1
 # print(atb)
 
-summa = 0
-for x in range(1, 1000):
-    if x % 3 == 0:
-      summa += x
-    elif x % 5 == 0:
-       summa += x
+"""
 
-print(summa)
+9. uzd
+
+"""
+
+# summa = 0
+# for x in range(1, 1000):
+#     if x % 3 == 0:
+#       summa += x
+#     elif x % 5 == 0:
+#        summa += x
+
+# print(summa)
 
 
+"""
+
+10. uzd
+
+"""
+
+# sk1 = 0
+# sk2 = 1
+# summa = 0
+
+# while sk2 <= 4000000:
+#     if sk2 % 2 == 0:
+#         summa += sk2
+#     sk1, sk2 = sk2, sk1 + sk2
+
+# print(summa)
+
+"""
+
+11. uzd
+
+"""
+
+# burti = "abcdefghijklmnopqrstuvwxyz"
+
+# for burts1 in burti:
+#     for burts2 in burti:
+#         print(burts1 + burts2)
+
+"""
+
+12. uzd
+
+"""
+
+skaitlis = float(input("Ievadi skaitli: "))
+pakape = 2 ** skaitlis
+print(2 ** skaitlis)
