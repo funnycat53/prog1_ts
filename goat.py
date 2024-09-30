2638f26c67df1dc34815589659cb11574bece17f
funnycat53 <patriks.jkb@gmail.com>
2024-09-30 13:27:26 +0300

    Update goat.py

--- goat.py
+++ goat.py
@@ -7,11 +7,9 @@ Izveidot programmu, kas aprēķina pirkumu bez pvn un pašu pvn vērtību
 
 """
 
+# def uzdevums1():
 
-def uzdevums1(pvn, cikIztereja, bezPVN):
-    print("Summa bez PVN:", bezPVN)
-    bezPVN = cikIztereja * pvn
-uzdevums1(0.79, float(input("Cik jūs naudu iztērējāt?")), )
+
 """
 
 Izveidot programmu, kur lietotājs secīgi var pabeigt 5 teikumus
@@ -28,14 +26,16 @@ Izveidot programmu, kas pārbauda vai lietotājam ir vairāk kā 18 gadi (tikai 
 
 """
 
-def uzdevums3():
-    gads = 2024
-    dzGads = float(input("Jūsu dzimšanas gads: "))
+# def uzdevums3(dzGads, gads = 2024):
+#     if gads - dzimsanas_gads == 18: 
+#         print("Jums ir 18 gadi")
+#     elif gads - dzimsanas_gads > 18:
+#         print("Jums ir vairāk nekā 18 gadi")
+#     else:
+#         print("Jums nav 18 gadu")
 
-    if gads - dzGads >= 18:
-        print("Lietotājam ir 18 gadu")
-    else:
-        print("Lietotājam nav 18 gadu")
+# dzimsanas_gads = int(input("Kurā gadā jūs pidzimāt?"))
+# uzdevums3(dzimsanas_gads)
 
 """
 
@@ -43,56 +43,61 @@ Izveidot programmu, kur 4 reizes var ievadīt kustības, tiek izvadīts virziens
 
 """
 
-def uzdevums4():
-    key = input("Ievadi w, a, s vai d: ")
+# def uzdevums4(key):
+#     if ievade == "w":
+#         print("Uz priekšu")
+#     elif ievade == "a":
+#         print("Pa kreisi")
+#     elif ievade == "d":
+#         print("Pa labi")
+#     elif ievade == "s":
+#         print("Atpakaļ")
+#     else:
+#         print("Kļūda")
 
-    if key == "w":
-        print("Uz priekšu")
-    elif key == "a":
-        print("Pa kreisi")
-    elif key == "d":
-        print("Pa labi")
-    elif key == "s":
-        print("Atpakaļ")
-    else:
-        print("Kļūda")
+# ievade = input("Ievadi w, a, s vai d: ")
+# uzdevums4(ievade)
+    
 
 """
 
 Izveidot programmu, kuru var atkārtot vairākas reizes
 
 """
-def uzdevums5():
-    while True:
-        user_input = input("Vai vēlies atkārtot šo programmu? y/n?")
+# def uzdevums5(ievade):
+#     while True:
+#         user_input = input("Vai vēlies atkārtot šo programmu? y/n?")
 
-        if user_input == "n":
-            print("Programma beidzās")
-            break
-        elif user_input == "y":
-            print("Programma turpinās")
-        else:
-            print("Nepareiza ievade, lūdzu ierakstiet y vai n")
+#         if user_input == "n":
+#             print("Programma beidzās")
+#             break
+#         elif user_input == "y":
+#             print("Programma turpinās")
+#         else:
+#             print("Nepareiza ievade, lūdzu ierakstiet y vai n")
 
+# user_input = input("Vai vēlies atkārtot šo programmu? y/n?")
+# uzdevums5(user_input)
 """
 
 Izveidot programmu, kur jāuzmin slēptais skaitlis (1-10)
 
 """
 
-def uzdevums6():
-    skaitlis = random.randint(1, 10)
+# def uzdevums6(random_skaitlis):
+#     while True:
 
-    while True:
+#         i = int(input("Ievadi savu minējumu: "))
+#         if i < skaitlis:
+#             print("Lielāks")
+#         elif i > skaitlis:
+#             print("Mazāks")
+#         else:
+#             print("Jūs uzminējāt")
+#             break
 
-        i = int(input("Ievadi savu minējumu: "))
-        if i < skaitlis:
-            print("Lielāks")
-        elif i > skaitlis:
-            print("Mazāks")
-        else:
-            print("Jūs uzminējāt")
-            break
+# skaitlis = random.randint(1, 10)
+# uzdevums6(skaitlis)
 
 """
 
@@ -100,19 +105,19 @@ Izveidot programmu, kur tiek izvadīti visi skaitļi, kas dalās ar ievadīto sk
 
 """
 
-def uzdevums7():
-    skaitlis = int(input("Ievadiet savu skaitli: "))
+# def uzdevums7(ievaditais_skaitlis):
+#     def atrast(cipars):
+#         dalitaji = []
+#         for i in range(1, cipars + 1):
+#             if cipars % i == 0:
+#                 dalitaji.append(i)
+#         return dalitaji
 
-    def atrast(cipars):
-        dalitaji = []
-        for i in range(1, cipars + 1):
-            if cipars % i == 0:
-                dalitaji.append(i)
-        return dalitaji
+#     dalitaji = atrast(skaitlis) 
+#     print(f"Skaitlis {skaitlis} dalās ar: {dalitaji}")
 
-    dalitaji = atrast(skaitlis) 
-    print(f"Skaitlis {skaitlis} dalās ar: {dalitaji}")
-
+# skaitlis = int(input("Ievadiet savu skaitli: "))
+# uzdevums7(skaitlis)
 
 """
 
@@ -120,15 +125,16 @@ Izveidot programmu, kur lietotājs ievada vārdu un burtu.
 Tiek izvadīts, cik norādīto burtu ir dotajā vārdā
 
 """
+# def uzdevums8(word, letter):
+#     atb = 0
+#     for x in vards:
+#         if x == burts:
+#             atb+= 1
+#     print(atb)
 
 # vards = input("Ievadi vārdu: ")
 # burts = input("Ievadi burtu: ")
-
-# atb = 0
-# for x in vards:
-#     if x == burts:
-#         atb+= 1
-# print(atb)
+# uzdevums8(vards, burts)
 
 """
 
@@ -136,14 +142,15 @@ Tiek izvadīts, cik norādīto burtu ir dotajā vārdā
 
 """
 
-# summa = 0
-# for x in range(1, 1000):
-#     if x % 3 == 0:
-#       summa += x
-#     elif x % 5 == 0:
-#        summa += x
+def uzdevums9(summers):
+    summa = 0
+    for x in range(1, 1000):
+        if x % 3 == 0:
+            summa += x
+        elif x % 5 == 0:
+            summa += x
 
-# print(summa)
+            print(uzdevums9(summa))
 
 
 """
